f959aec04f9412c7c360c55b6556d1537c5ada64
David Schreck <dschreck2@liberty.edu>
2016-02-29 23:56:03 -0500

    Can create customers

--- app/sections/customers/create-customer/create-customer-controller.js
+++ app/sections/customers/create-customer/create-customer-controller.js
@@ -2,11 +2,15 @@
 
 angular.module('biofuels.sections.createCustomer.controller', [])
   .controller('createCustomerCtrl',
-    function ($log) {
+    function ($log,
+              customerService,
+              $state,
+              ROUTES
+    ) {
       (function (vm) {
         $log.debug('This is from the home page');
         vm.customerData = {
-          PONumbers: []
+          PONumbers: [],
         };
         vm.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
         'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
@@ -25,6 +29,18 @@ angular.module('biofuels.sections.createCustomer.controller', [])
 
         vm.createCustomer = function () {
           $log.debug('Create this customer');
+
+          vm.formError = '';
+          vm.formResponse = '';
+          customerService.createCustomer(2, vm.customerData.name, vm.customerData.code, vm.customerData.contactName1, vm.customerData.contactName2, vm.customerData.billingName,
+                                         vm.customerData.address, vm.customerData.address2, vm.customerData.city, vm.customerData.state,
+                                         Number(vm.customerData.postalCode), vm.customerData.phoneNumber, vm.customerData.email).then(function (data) {
+            vm.formResponse = 'Successfully made customer!';
+            $state.go(ROUTES.CUSTOMERS);
+          }).catch(function (err) {
+            vm.formError = err.error;
+          });
         };
+
       })(this);
     });
